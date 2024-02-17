820b0fc0d7bc00426d4fb1a3226ed2d0cb6b3040
Mohammad-Bajaa <75749165+Mohammad-Bajaa@users.noreply.github.com>
2024-02-17 22:27:36 +0300

    Navbar1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react"
 import ReactLogo from "../images/Bajaa/logo.png"
 import './Navbar.css'
+
   export default function Navbar (props) {
+    function collapseBar(){
+      const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+      navbarLinks.classList.toggle('active')
+    }
     return (
         <header className={props.darkMode ? "nav dark-nav" : "nav"}>
           {/* <navbar className="my-nav">
@@ -11,12 +16,29 @@ import './Navbar.css'
               <li className="nav-item">Contact Me</li>
             </ul>
           </navbar> */}
-            <img className="react-logo" src = {ReactLogo} alt="react-logo"/>
-            <h1 className={props.darkMode ? "project-name dark-h1" : "project-name"}> Work & Experience </h1>
+            <div className="title-block">
+              <img className="react-logo" src = {ReactLogo} alt="react-logo"/>
+              <h1 className={props.darkMode ? "project-name dark-h1" : "project-name"}> Work & Experience </h1>
+            </div>
+            <a onClick={collapseBar} className="toggle-button">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </a>
             {/* <h3 className="project-description">Mohammad Hussain Bajaa</h3> */}
-            <div className="dark-light-button" >
-              <input type="checkbox" id="darkmode-toggle"/>
-              <label id="darkmode-toggle-label" for="darkmode-toggle" onClick={props.toggleDarkMode}/>    
+            <div className="navbar-links">
+              
+              <ul className="nav-list">
+                <li className="nav-item"> <a href="#">My CV</a></li>
+                <li className="nav-item"> <a href="#">About Developer</a></li>
+                <li className="nav-item"> <a href="#">Contact Me</a></li>
+                <li className="nav-item">
+                  <div className="dark-light-button" >
+                    <input type="checkbox" id="darkmode-toggle"/>
+                    <label id="darkmode-toggle-label" for="darkmode-toggle" onClick={props.toggleDarkMode}/>    
+                  </div>
+                </li>
+              </ul>
             </div>
         </header>
     )
